5068228ec7443863756a70b49bf657b6187174e2
Amol Jagtap <ajfsdeveloper@gmail.com>
2018-10-01 12:40:43 +0530

    Refactor Follow component

--- src/components/Follow.jsx
+++ src/components/Follow.jsx
@@ -4,6 +4,14 @@ const styles = {
   margin: '5px 10px'
 }
 
+const followData = [
+  { site: 'facebook' },
+  { site: 'twitter' },
+  { site: 'linkedin' },
+  { site: 'google-plus' },
+  { site: 'pinterest' }
+]
+
 const Follow = () => {
   return (
     <section className="section blue darken-2 white-text center">
@@ -12,21 +20,11 @@ const Follow = () => {
           <div className="col s12">
             <h4>Follow Travelville</h4>
             <p>Follow us on social media for special offers</p>
-            <a href="#!" className="white-text">
-              <i className="fab fa-facebook fa-4x" style={styles} />
-            </a>
-            <a href="#!" className="white-text">
-              <i className="fab fa-twitter fa-4x" style={styles} />
-            </a>
-            <a href="#!" className="white-text">
-              <i className="fab fa-linkedin fa-4x" style={styles} />
-            </a>
-            <a href="#!" className="white-text">
-              <i className="fab fa-google-plus fa-4x" style={styles} />
-            </a>
-            <a href="#!" className="white-text">
-              <i className="fab fa-pinterest fa-4x" style={styles} />
-            </a>
+            {followData.map(data => (
+              <a href="#!" className="white-text" key={data.site}>
+                <i className={`fab fa-${data.site} fa-4x`} style={styles} />
+              </a>
+            ))}
           </div>
         </div>
       </div>
