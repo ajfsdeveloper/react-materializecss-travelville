2cb18d05009bfe4c26c12c8365f9b929ac4bdb17
Amol Jagtap <ajfsdeveloper@gmail.com>
2018-10-01 11:58:18 +0530

    Refactor code

--- src/components/IconBoxes.jsx
+++ src/components/IconBoxes.jsx
@@ -1,42 +1,30 @@
 import React from 'react'
 
+const boxesData = [
+  { iconClass: 'room', text: 'Pick Where' },
+  { iconClass: 'store', text: 'Travel Shop' },
+  { iconClass: 'airplanemode_active', text: 'Fly Cheap' }
+]
+
 const IconBoxes = () => {
   return (
     <section className="section section-icons grey lighten-4 center">
       <div className="container">
         <div className="row">
-          <div className="col s12 m4">
-            <div className="card-panel">
-              <i className="material-icons large blue-text">room</i>
-              <h4>Pick Where</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-                blanditiis.
-              </p>
+          {boxesData.map(box => (
+            <div className="col s12 m4" key={box.iconClass}>
+              <div className="card-panel">
+                <i className="material-icons large blue-text">
+                  {box.iconClass}
+                </i>
+                <h4>{box.text}</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
+                  blanditiis.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="col s12 m4">
-            <div className="card-panel">
-              <i className="material-icons large blue-text">store</i>
-              <h4>Travel Shop</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-                blanditiis.
-              </p>
-            </div>
-          </div>
-          <div className="col s12 m4">
-            <div className="card-panel">
-              <i className="material-icons large blue-text">
-                airplanemode_active
-              </i>
-              <h4>Fly Cheap</h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
-                blanditiis.
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
